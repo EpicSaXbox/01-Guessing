bf85c886f1bdc4d2a250612ab9f6b17817db180b
EpicSaXbox <59945164+EpicSaXbox@users.noreply.github.com>
2020-01-27 13:56:11 -0500

    Creates main.py and fulfills the requirements of the assignment

---
+++ main.py
@@ -0,0 +1,36 @@
+import sys, random
+
+assert sys.version_info >= (3,7), "This script requires at least Python 3.7"
+
+
+quit = False
+range = 100
+
+while not quit:
+    random_number = random.randint(1,range)
+    count = 1
+    number = -1
+    while number != random_number:
+        number= input("Please guess a number between 1 and {}: ".format(range))
+        if not number.isdigit():
+            print("Try to guess the number!")
+        else:
+            number = int(number)
+            count = count + 1 
+            print("I'm afraid that answer is incorrect")
+            if number > random_number:
+                print("Too high!")
+            elif number < random_number:
+                print("Too low!")
+    print("Outstanding job!")
+    print("You guessed it in {} tries!".format(count))
+    play_again = input("\nWould you like to try and win agian (yes or no)?")
+    play_again = play_again.lower()
+    if play_again == "yes" or play_again == "y":
+        quit = False
+    else:
+        quit = True
+
+print("\n\nThank you so much for playing the guessing game! Have a great day!")
+
+
